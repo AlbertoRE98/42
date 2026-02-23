79df90c10221d11876ceb4d2dc23c5f36abed920
Albertore98 <albertramo98@gmail.com>
2026-02-23 19:46:14 +0100

    hecho el memset

---
+++ rank00/Libft/LIBC_functions/ft_memset.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+void *ft_memset(void *b, int c, size_t len)
+{
+    unsigned char *ptr = (unsigned char *)b;
+    unsigned char value = (unsigned char)c;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        ptr[i] = value;
+    }
+
+    return b;
+}
