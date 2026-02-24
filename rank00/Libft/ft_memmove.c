8f442033cbc9e69bf139fd03124a5a98ecb5ef89
Albertore98 <albertramo98@gmail.com>
2026-02-24 20:11:27 +0100

    hechos los to lower y to upper, y hecho el memmove

--- rank00/Libft/ft_memmove.c
+++ rank00/Libft/ft_memmove.c
@@ -2,21 +2,27 @@
 
 #include "libft.h"
 
-void    *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-    /* declare 2 temporary pointer for src and dst */
-    unsigned char *tmp_dst;
-    unsigned char *tmp_src;
-    int            counter;
-    /* declare a counter */
-    /* check if both src and dst are NULL */
-    if (src == NULL && dst == NULL)
-        return (NULL);
-    /* make dst tmp pointer equal to dst converted to char * */
-    /* make src tmp pointer equal to src converted to char * */
-    /* if src and dst are overlapping */
-        /* loop while len is greater than 0 and copy src into dst */
-    /* if src and dst are not overlapping */
-        /* loop while our counter is less than len and copy src into dst */
-    /* return dst */
+	unsigned char *tmp_dst;
+	unsigned char *tmp_src;
+
+	if (src == NULL && dst == NULL)
+		return (NULL);
+	tmp_dst = (unsigned char *) dst;
+	tmp_src = (unsigned char *) src;
+
+	if (dst < src)
+	{
+		while (len--)
+			*(tmp_dst++) = *(tmp_src++);
+	}
+	else if (dst > src)
+	{   
+		tmp_dst += len;
+		tmp_src += len;
+		while(len--)
+			*(--tmp_dst) = *(--tmp_src);
+	}
+	return (dst);
 }
