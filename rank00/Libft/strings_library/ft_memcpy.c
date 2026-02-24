de7da3779b31e33eda7d6894a819dda2de8572d7
Albertore98 <albertramo98@gmail.com>
2026-02-24 21:04:48 +0100

    cambiado el memcpy

--- rank00/Libft/strings_library/ft_memcpy.c
+++ rank00/Libft/strings_library/ft_memcpy.c
@@ -5,20 +5,18 @@
 void *ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char       *temp_destiny;
-	const unsigned char *temp_source;
+	unsigned char 		*temp_source;
 
 	if (dst == NULL && src == NULL)
 	{
 		return (NULL);
 	}
-	if (n == 0)
-		return (dst);
-		temp_destiny = (unsigned char*) dst;
-		temp_source = (unsigned char*) src;
-		while (n > 0)
-		{
-			*(temp_destiny++) = *(temp_source++);
-			n--;
-		}
+	temp_destiny = (unsigned char *)dst;
+    temp_source = (const unsigned char *)src;
+    
+    while (n--)
+    {
+        *(temp_destiny++) = *(temp_source++);
+    }
 	return (dst);
 }
