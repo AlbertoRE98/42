d6fcf4b5f73c5d000b3fd09bc85b02cef4b7793d
Albertore98 <albertramo98@gmail.com>
2026-02-24 14:19:17 +0100

    hecho cambios y memcpy

--- rank00/Libft/LIBC_functions/ft_memcpy.c
+++ rank00/Libft/LIBC_functions/ft_memcpy.c
@@ -2,3 +2,19 @@
 
 #include "libft.h"
 
+void *ft_memcpy(void *dst, const void *src, size_t n)
+{
+    unsigned char *d = (unsigned char *)dst;
+    const unsigned char *s = (const unsigned char *)src;
+
+    if (dst == NULL && src == NULL)
+        return (NULL);
+    while (n > 0)
+    {
+        *d = *s;
+        d++;
+        s++;
+        n--;
+    }
+    return dst;
+}
