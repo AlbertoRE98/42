5f4afaa192ae16b1deae50a1b9a8dd5af39bb3c1
Albertore98 <albertramo98@gmail.com>
2026-02-25 16:55:46 +0100

    hecho el memccpy

---
+++ rank00/Libft/src/mem/ft_memccpy.c
@@ -0,0 +1,24 @@
+
+#include "libft.h"
+
+void		*ft_memccpy(void *dest, const void *src, int c, size_t n)
+{  
+	unsigned char		*aux_dest;
+	const unsigned char	*aux_src;
+	size_t				index;
+	unsigned char 		ucaux;
+
+	ucaux = (unsigned char)c;
+	aux_dest = (unsigned char *)dest;
+	aux_src = (const unsigned char *)src;
+	index = 0;
+	while (index++ < n)
+	{
+		*aux_dest = *aux_src;
+		if (*aux_dest == ucaux)
+			return (aux_dest + 1) ;
+		aux_dest++;
+		aux_src++;
+	}
+	return (NULL);
+}
