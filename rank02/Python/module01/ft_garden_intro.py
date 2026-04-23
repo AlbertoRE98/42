f2232a734a52826421c2d6f7b7f68000642ad4d9
Alberto Ramos Escalera <aramos-e@c1r4s4.42malaga.com>
2026-04-23 15:02:00 +0200

    hechos los dos primeros del python 01

---
+++ rank02/Python/module01/ft_garden_intro.py
@@ -0,0 +1,11 @@
+
+name = "Rose"
+Height = "25cm"
+Age = "30 days"
+
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {Height}")
+    print(f"Age: {Age}")
+    print("=== End of Program ===")
